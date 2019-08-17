933a0029b3cbf30a8e4f3d45b736468d71c9983a
vyshakg <vyshak.g.gowda@gmail.com>
2019-08-17 21:50:46 +0530

    added google analiytics

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
+import ReactGA from "react-ga";
 import LandingPage from "./pages/LandingPage";
 import { CustomThemeProvider } from "./themes/themeContext";
-
 class App extends Component {
   render() {
+    ReactGA.initialize("UA-145894494-1");
+    ReactGA.pageview("/");
     return (
       <CustomThemeProvider>
         <LandingPage />
